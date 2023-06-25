51f96ac973e9be530a098094cd2114c189018a0f
Mayankbohemia <b160024@nitsikkim.ac.in>
2023-06-25 22:32:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${page}`
+        );
+        const data = response.data;
+        const newPosts = data.hits.map(hit => ({
+          title: hit.title,
+          url: hit.url,
+          created_at: hit.created_at,
+          author: hit.author
+        }));
+        setPosts(oldPosts => [...oldPosts, ...newPosts]);
+        setPage(oldPage => oldPage + 1);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    };
+
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 10000);
+
+    if(page === 0){
+       fetchData();
+    }
+
+   
+
+  
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>URL</th>
+            <th>Created At</th>
+            <th>Author</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((post, index) => (
+            <tr key={index}>
+              <td>{post.title}</td>
+              <td>{post.url}</td>
+              <td>{post.created_at}</td>
+              <td className='gap'>{post.author}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
